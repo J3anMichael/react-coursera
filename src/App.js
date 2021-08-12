71ad4f7f114e67e92b3bc1d7be24e91b8a36ad0c
Jean Michael <jeanmmclaudino@gmail.com>
2021-08-12 10:33:37 -0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 
@@ -8,20 +8,11 @@ class App extends Component {
   render(){
     return(
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar dar color="primary">
+        <div className="container">
+            <NavbarBrand href="/">Ristorant Con Fusion</NavbarBrand>
+        </div>
+      </Navbar> 
     </div>
     )
   }
